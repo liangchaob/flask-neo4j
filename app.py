b9110a3e05a6c7931ace9c36ab3f6a6a105bdab1
liangchaob <liangchaob@163.com>
2023-12-10 23:21:20 +0800

    对代码进行优化，补充必要的细节

--- app.py
+++ app.py
@@ -1,79 +1,105 @@
 from flask import Flask, render_template, request, redirect, url_for
-from neo4j import GraphDatabase
+from neo4j import GraphDatabase, exceptions
 from config import NEO4J_USERNAME, NEO4J_PASSWORD
-
-# Neo4j 数据库连接
-uri = "bolt://localhost:7687"
-driver = GraphDatabase.driver(uri, auth=(NEO4J_USERNAME, NEO4J_PASSWORD)) # 使用你的用户名和密码
-
 
 app = Flask(__name__)
 
+# 初始化 Neo4j 数据库连接
+uri = "bolt://localhost:7687"
+try:
+    driver = GraphDatabase.driver(uri, auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
+except exceptions.ServiceUnavailable as e:
+    raise SystemExit("Failed to connect to Neo4j database.") from e
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """渲染主页，显示所有节点，并提供创建新节点的功能。"""
     if request.method == 'POST':
         name = request.form['name']
         label = request.form['label']
+        try:
+            with driver.session() as session:
+                session.run(f"CREATE (p:{label} {{name: $name}})", name=name)
+        except Exception as e:
+            # 处理可能的数据库异常
+            print(f"Error creating node: {e}")
+
+    # 检索所有节点及其标签
+    try:
         with driver.session() as session:
-            session.run(f"CREATE (p:{label} {{name: $name}})", name=name)
-    
-    # 省略其他代码...
-    with driver.session() as session:
-        result = session.run("MATCH (n) RETURN ID(n) AS id, n.name, labels(n)")
-        nodes = [{"id": record["id"], "name": record["n.name"], "labels": record["labels(n)"]} for record in result]
+            result = session.run("MATCH (n) RETURN ID(n) AS id, n.name, labels(n)")
+            nodes = [{"id": record["id"], "name": record["n.name"], "labels": record["labels(n)"]} for record in result]
+    except Exception as e:
+        print(f"Error retrieving nodes: {e}")
+        nodes = []
 
     return render_template('index.html', nodes=nodes)
 
 @app.route('/show/<int:id>', methods=['GET'])
 def show(id):
-    with driver.session() as session:
-        result = session.run("MATCH (n) WHERE ID(n) = $id RETURN n, labels(n)", id=id)
-        data = result.single()
-        if data:
-            node = data["n"]
-            labels = data["labels(n)"]
-        else:
-            node, labels = None, []
+    """显示特定节点的详细信息。"""
+    try:
+        with driver.session() as session:
+            result = session.run("MATCH (n) WHERE ID(n) = $id RETURN n, labels(n)", id=id)
+            data = result.single()
+            if data:
+                node = data["n"]
+                labels = data["labels(n)"]
+            else:
+                node, labels = None, []
+    except Exception as e:
+        print(f"Error retrieving node: {e}")
+        node, labels = None, []
 
     return render_template('show.html', node=node, labels=labels)
 
-
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
+    """更新特定节点的信息。"""
     if request.method == 'POST':
         new_name = request.form['new_name']
-        with driver.session() as session:
-            session.run("MATCH (n) WHERE ID(n) = $id SET n.name = $new_name", id=id, new_name=new_name)
-        # 其余代码不变...
+        try:
+            with driver.session() as session:
+                session.run("MATCH (n) WHERE ID(n) = $id SET n.name = $new_name", id=id, new_name=new_name)
+        except Exception as e:
+            print(f"Error updating node: {e}")
 
         return redirect(url_for('index'))
 
     # 获取当前节点信息以用于表单预填充
-    with driver.session() as session:
-        result = session.run("MATCH (n) WHERE ID(n) = $id RETURN n", id=id)
-        node = result.single()["n"]
+    try:
+        with driver.session() as session:
+            result = session.run("MATCH (n) WHERE ID(n) = $id RETURN n", id=id)
+            node = result.single()["n"]
+    except Exception as e:
+        print(f"Error retrieving node for update: {e}")
+        node = None
 
     return render_template('update.html', node=node)
 
-
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete(id):
-    with driver.session() as session:
-        session.run("MATCH (n) WHERE ID(n) = $id DETACH DELETE n", id=id)
-        
-    return redirect(url_for('index'))
+    """删除特定节点。"""
+    try:
+        with driver.session() as session:
+            session.run("MATCH (n) WHERE ID(n) = $id DETACH DELETE n", id=id)
+    except Exception as e:
+        print(f"Error deleting node: {e}")
 
+    return redirect(url_for('index'))
 
 @app.route('/labels', methods=['GET'])
 def list_labels():
-    with driver.session() as session:
-        result = session.run("CALL db.labels()")
-        labels = [record["label"] for record in result]
+    """显示数据库中所有唯一的标签。"""
+    try:
+        with driver.session() as session:
+            result = session.run("CALL db.labels()")
+            labels = [record["label"] for record in result]
+    except Exception as e:
+        print(f"Error retrieving labels: {e}")
+        labels = []
+
     return render_template('labels.html', labels=labels)
 
-
-
-
-
 if __name__ == '__main__':
-    app.run(debug=True,port=5001)
+    app.run(debug=True, port=5001)
