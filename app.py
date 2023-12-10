6173897f2f5ee28138ec136225e104ad8c369003
liangchaob <liangchaob@163.com>
2023-12-10 23:12:04 +0800

    调整为查看全部节点

--- app.py
+++ app.py
@@ -13,34 +13,67 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         name = request.form['name']
+        label = request.form['label']
         with driver.session() as session:
-            session.run("CREATE (p:Person {name: $name})", name=name)
+            session.run(f"CREATE (p:{label} {{name: $name}})", name=name)
     
+    # 省略其他代码...
     with driver.session() as session:
-        result = session.run("MATCH (p:Person) RETURN p.name")
-        names = [record["p.name"] for record in result]
+        result = session.run("MATCH (n) RETURN ID(n) AS id, n.name, labels(n)")
+        nodes = [{"id": record["id"], "name": record["n.name"], "labels": record["labels(n)"]} for record in result]
 
-    return render_template('index.html', names=names)
+    return render_template('index.html', nodes=nodes)
 
-@app.route('/details/<name>', methods=['GET'])
-def details(name):
-    return render_template('details.html', name=name)
+@app.route('/show/<int:id>', methods=['GET'])
+def show(id):
+    with driver.session() as session:
+        result = session.run("MATCH (n) WHERE ID(n) = $id RETURN n, labels(n)", id=id)
+        data = result.single()
+        if data:
+            node = data["n"]
+            labels = data["labels(n)"]
+        else:
+            node, labels = None, []
 
-@app.route('/update/<name>', methods=['GET', 'POST'])
-def update(name):
+    return render_template('show.html', node=node, labels=labels)
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
     if request.method == 'POST':
         new_name = request.form['new_name']
         with driver.session() as session:
-            session.run("MATCH (p:Person {name: $name}) SET p.name = $new_name", name=name, new_name=new_name)
-        return redirect(url_for('index'))
-    
-    return render_template('update.html', name=name)
+            session.run("MATCH (n) WHERE ID(n) = $id SET n.name = $new_name", id=id, new_name=new_name)
+        # 其余代码不变...
 
-@app.route('/delete/<name>', methods=['POST'])
-def delete(name):
+        return redirect(url_for('index'))
+
+    # 获取当前节点信息以用于表单预填充
     with driver.session() as session:
-        session.run("MATCH (p:Person {name: $name}) DELETE p", name=name)
+        result = session.run("MATCH (n) WHERE ID(n) = $id RETURN n", id=id)
+        node = result.single()["n"]
+
+    return render_template('update.html', node=node)
+
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    with driver.session() as session:
+        session.run("MATCH (n) WHERE ID(n) = $id DETACH DELETE n", id=id)
+        
     return redirect(url_for('index'))
+
+
+@app.route('/labels', methods=['GET'])
+def list_labels():
+    with driver.session() as session:
+        result = session.run("CALL db.labels()")
+        labels = [record["label"] for record in result]
+    return render_template('labels.html', labels=labels)
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=5001)
